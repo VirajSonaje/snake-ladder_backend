88083aeb5812ea913387a2dd6127a48cb35c96ca
VirajSonaje <virajsonaje@gmail.com>
2024-05-08 20:21:28 -0700

    WebSocket rooms
    Websocket to handle rooms and users.
    Events to send dice roll data.

---
+++ server.js
@@ -0,0 +1,39 @@
+const { networkInterfaces } = require('os');
+
+const nets = networkInterfaces();
+const results = Object.create(null); // Or just '{}', an empty object
+
+for (const name of Object.keys(nets)) {
+    for (const net of nets[name]) {
+
+        const familyV4Value = typeof net.family === 'string' ? 'IPv4' : 4
+        if (net.family === familyV4Value && !net.internal) {
+            if (!results[name]) {
+                results[name] = [];
+            }
+            results[name].push(net.address);
+        }
+    }
+}
+
+const io = require('socket.io')(3500, {
+    cors: {
+        origin: ['http://localhost:3000',
+                 'http://'+results.en0+':3000']
+    }
+});
+
+let allRooms = new Map();
+
+io.on('connection', socket => {
+    console.log(socket.id);
+    socket.on('send-roll', (data) => {
+        socket.to(data.Room).emit("recieve-roll", data);
+    })
+
+    socket.on('join-room', room => {
+        allRooms[room]
+        console.log("joined %s", room);
+        socket.join(room);
+    })
+});
